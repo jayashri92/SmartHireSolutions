f938bfc7f3a1b3b41f533b6811544528c9cdcd3c
jayashri92 <jayashri92raut@gmail.com>
2025-02-14 15:29:39 +0530

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { OurServicesComponent } from './our-services/our-services.component';
+import { TrainingComponent } from './training/training.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent },
+  {path:'about',component:AboutComponent},
+  {path:'ourService',component:OurServicesComponent},
+  {path:'training',component:TrainingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { OurServicesComponent } from './our-services/our-services.component';
+import { BannerComponent } from './banner/banner.component';
+import { TrainingComponent } from './training/training.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    OurServicesComponent,
+    BannerComponent,
+    TrainingComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
